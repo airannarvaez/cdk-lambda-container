be891bc8a1305ced7b6d93ea12877ece9673042f
airan narvaez <anarvae@bancolombia.com.co>
2023-07-08 17:17:07 -0300

    feat: :art: prepare code in main files

--- lib/cdk-lambda-container-stack.ts
+++ lib/cdk-lambda-container-stack.ts
@@ -1,16 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkLambdaContainerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkLambdaContainerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaContainerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
