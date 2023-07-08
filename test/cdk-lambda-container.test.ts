05c73564ae14a357eac83cc2dae89605f5b104bc
airan narvaez <anarvae@bancolombia.com.co>
2023-07-08 18:33:46 -0300

    test: add unit tests

--- test/cdk-lambda-container.test.ts
+++ test/cdk-lambda-container.test.ts
@@ -1,17 +1,43 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkLambdaContainer from '../lib/cdk-lambda-container-stack';
+import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { CdkLambdaContainerStack } from '../lib/cdk-lambda-container-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-lambda-container-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkLambdaContainer.CdkLambdaContainerStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe("CdkLambdaContainerStack", () => {
+    let app: App;
+    let cdkLambdaContainerStack: Stack;
+    let template: Template;
+
+    beforeEach(() => {
+        app = new App();
+
+        // Create the CdkLambdaContainerStack.
+        cdkLambdaContainerStack = new CdkLambdaContainerStack(app, 'CdkLambdaContainerStack');
+
+        // Prepare the stack for assertions.
+        template = Template.fromStack(cdkLambdaContainerStack);
+    });
+
+    test('Has two functions', () => {
+        // Counts functions...
+        template.resourceCountIs('AWS::Lambda::Function', 2);
+    });
+
+    test("Has Hello Function", () => {
+        // Assert it creates the function with the correct properties...
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            ImageConfig: {
+                Command: ['hello.lambdaHandler']
+            }
+        });
+    });
+
+    test("Has PDF Function", () => {
+        // Assert it creates the function with the correct properties...
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            ImageConfig: {
+                Command: ['pdf.lambdaHandler']
+            }
+        });
+    });
 });
